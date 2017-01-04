e52ebc4f01ccc8a1b3ddf6439ffd6f092507d224
Frédéric Marcel Falliere <frederic.falliere@gmail.com>
2017-01-04 13:56:00 +0100

    remember me

--- js/controllers/loginCtrl.js
+++ js/controllers/loginCtrl.js
@@ -10,47 +10,61 @@ angular.module('spaceDogCockpit.controllers')
   };
 
   var loginSuccess = function(data){
-    console.log(data);
+
     if (data.credentials.roles.indexOf('admin') == -1) {
+
       $scope.error = "Vous n'avez pas le rôle [admin] suffisant pour vous connecter"
+
       $rootScope.hideLoading();
+
     } else {
-      // $state.go('app.home')
+
       $state.go('app.schemas')
+
       $rootScope.hideLoading();
-      // $state.go('app.admins')
-      // $state.go('app.admin', {id:"AVe-NE9mUm-r44yDpKG8", credential_id:"AVe-NE7hUm-r44yDpKG6" })
+
     }
   }
 
-  // if (AuthService.hasAccessToken()) {
-  //   $rootScope.showLoading();
-  //   AuthService.tryLogin().then(function(response){
-  //     loginSuccess(response, true)
-  //   }, function(e){
-  //     $scope.error = ErrorFormatService.format(e);
-  //     AuthService.forgetAccessToken();
-  //     $rootScope.hideLoading();
-  //   })
-  // }
+
+  var loginError = function(err) {
+    $scope.error = ErrorFormatService.format(err);
+    
+    SpaceDog.Credentials.forget();
+
+    $rootScope.hideLoading()
+  }
+
+  var loginCallback = function(err, data) {
+
+    if (err!=null) {
+
+      loginError(err)
+
+    } else {
+
+      loginSuccess(data)
+    }
+  }
+
+
+  if (SpaceDog.Credentials.canTryLogin()) {
+
+    $rootScope.showLoading()
+
+    SpaceDog.Credentials.loginWithSavedCredentials(loginCallback)
+  }
+
 
   $scope.login = function(){
+
     $scope.error = undefined;
+
     $rootScope.showLoading();
 
     SpaceDog.initialize($scope.loginData.backendId)
-    SpaceDog.Credentials.login($scope.loginData, function(err, data) {
-      if (err!=null) {
-        
-        $rootScope.hideLoading();
-        $scope.error = ErrorFormatService.format(err);
 
-      } else {
-          
-        loginSuccess(data)
-        
-      }
-    })
+    SpaceDog.Credentials.login($scope.loginData, loginCallback)
 
   }
 
